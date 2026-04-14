05bed4f1ea6c3313a66cd7563a6266e21119e10b
Diego Frigi <diegofrigi08@gmail.com>
2026-04-14 17:54:19 -0300

    Initial commit - calculator project

--- codigo1.py
+++ codigo1.py
@@ -1,18 +1,69 @@
-num1 = float(input("Enter a number: "))
-operator = input("Enter an operator(+, -, *, /): ")
-num2 = float(input("Enter another number: "))
+def get_valid_num(prompt_num):
+    while True:
+        number = input(prompt_num)
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result, 3))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result, 3))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result, 3))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 3))
-else:
-    print("You need to write an operator!")  
+        if number.lower() == "q":
+            return None
+        
+        try:
+            num = float(number)
+        except ValueError:
+            print("Enter a valid number")
+            continue
+
+        return num
+    
+def get_valid_operator():
+    while True:
+        operator = input("Enter a valid operator(+, -, *, /): ")
+
+        if operator in ["+", "-", "*", "/"]:
+            return operator
+        print("Enter a valid operator")
+        continue
+
+def calculate(op, a, b):
+        if op == "+":
+            return a + b
+        elif op == "-":
+            return a - b
+        elif op == "*":
+            return a * b
+        elif op == "/":
+            try: return a / b
+            except ZeroDivisionError:
+                print("second number cannot be zero")
+                return None
+                    
+while True:
+    number = get_valid_num("Enter a number(q to quit): ")
+
+    if number is None:
+        break
+
+    number2 = get_valid_num("Enter another number: ")
+
+    if number2 is None:
+        break
+
+    operator = get_valid_operator()
+
+    result = calculate(operator, number, number2)
+
+    if result is not None:
+        print(result)
+
+    
+
+
+
+    
+
+
+
+       
+       
+        
+        
+        
+    

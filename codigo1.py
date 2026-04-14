1425a9c34171119c14f6621c8d8f477ac70c4343
Diego Frigi <diegofrigi08@gmail.com>
2026-04-14 18:18:20 -0300

    refactor: reorganize code and add main function

--- codigo1.py
+++ codigo1.py
@@ -1,4 +1,4 @@
-def get_valid_num(prompt_num):
+def get_valid_number(prompt_num):
     while True:
         number = input(prompt_num)
 
@@ -20,7 +20,6 @@ def get_valid_operator():
         if operator in ["+", "-", "*", "/"]:
             return operator
         print("Enter a valid operator")
-        continue
 
 def calculate(op, a, b):
         if op == "+":
@@ -30,29 +29,35 @@ def calculate(op, a, b):
         elif op == "*":
             return a * b
         elif op == "/":
-            try: return a / b
+            try: 
+                return a / b
             except ZeroDivisionError:
                 print("second number cannot be zero")
                 return None
-                    
-while True:
-    number = get_valid_num("Enter a number(q to quit): ")
 
-    if number is None:
-        break
+def main():
+    print("=== SIMPLE CALCULATOR ===")         
 
-    number2 = get_valid_num("Enter another number: ")
+    while True:
+        number = get_valid_number("Enter a number(q to quit): ")
 
-    if number2 is None:
-        break
+        if number is None:
+            break
 
-    operator = get_valid_operator()
+        number2 = get_valid_number("Enter another number: ")
 
-    result = calculate(operator, number, number2)
+        if number2 is None:
+            break
 
-    if result is not None:
-        print(result)
+        operator = get_valid_operator()
 
+        result = calculate(operator, number, number2)
+
+        if result is not None:
+            print(f"Result is {result}")
+
+if __name__ == "__main__":
+    main()
     
 
 
